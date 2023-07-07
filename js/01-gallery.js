8311bff3ed253c43430dbf4e39cd31a62c58f89b
KostiantynSS <storoghenko94@gmail.com>
2023-07-07 22:52:18 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryList: document.querySelector(".gallery"),
+};
+
+refs.galleryList.addEventListener("click", showOriginalImage);
+
+const galleryListImages = galleryItems.map((image, index) => {
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+    li.innerHTML = `<a class="gallery__link" href="${image.original}"><img class='gallery__image' data-index='${index}' data-source='${image.original}'src='${image.preview}' alt='${image.description}'/> </a>`;
+    return li;
+});
+
+refs.galleryList.append(...galleryListImages);
+
+function showOriginalImage(event) {
+    if (event.target.tagName === "IMG") {
+        const imageIndex = event.target.dataset.index;
+        console.log(imageIndex);
+        const instance = basicLightbox.create(
+            `<img src='${galleryItems[imageIndex].original}' alt='${galleryItems[imageIndex].description}' >`
+        );
+
+        instance.show();
+    }
+}
+// //  <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
